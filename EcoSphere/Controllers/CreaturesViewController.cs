436d3f1d3f67fd7b75dad1d57714fecb95d84ef5
Deniz Coşkun <138766646+DnzCoskun@users.noreply.github.com>
2025-03-26 00:02:54 +0300

    hh

--- EcoSphere/Controllers/CreaturesViewController.cs
+++ EcoSphere/Controllers/CreaturesViewController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
-
+//merhaba togla
 namespace EcoSphere.Controllers
 {
     public class CreaturesViewController : Controller
