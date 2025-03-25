d9c807146fb61f02e7c67b3b7b78c988bc4c6d23
Yigitbdk <yigitbadik@gmail.com>
2025-03-26 00:06:56 +0300

    front

---
+++ ecosphere/wwwroot/js/mapdata.js
@@ -0,0 +1,767 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#0000",
+    background_transparent: "yes",
+    border_color: "#000000",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: "2",
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    TR01: {
+      name: "Adana",
+      color: "#5c0ade"
+    },
+    TR02: {
+      name: "Adiyaman",
+      color: "#000080"
+    },
+    TR03: {
+      name: "Afyonkarahisar",
+      color: "#4e9cf4"
+    },
+    TR04: {
+      name: "Agri",
+      color: "#0a4f0a"
+    },
+    TR05: {
+      name: "Amasya",
+      color: "#008080"
+    },
+    TR06: {
+      name: "Ankara",
+      color: "#DE3163"
+    },
+    TR07: {
+      name: "Antalya",
+      color: "#5c0ade"
+    },
+    TR08: {
+      name: "Artvin",
+      color: "#008080"
+    },
+    TR09: {
+      name: "Aydin",
+      color: "#4e9cf4"
+    },
+    TR10: {
+      name: "Balikesir",
+      color: "#b5bd0c"
+    },
+    TR11: {
+      name: "Bilecik",
+      color: "#b5bd0c"
+    },
+    TR12: {
+      name: "Bingöl",
+      color: "#0a4f0a"
+    },
+    TR13: {
+      name: "Bitlis",
+      color: "#0a4f0a"
+    },
+    TR14: {
+      name: "Bolu",
+      color: "#008080"
+    },
+    TR15: {
+      name: "Burdur",
+      color: "#5c0ade"
+    },
+    TR16: {
+      name: "Bursa",
+      color: "#b5bd0c"
+    },
+    TR17: {
+      name: "Çanakkale",
+      color: "#b5bd0c"
+    },
+    TR18: {
+      name: "Çankiri",
+      color: "#DE3163"
+    },
+    TR19: {
+      name: "Çorum",
+      color: "#008080"
+    },
+    TR20: {
+      name: "Denizli",
+      color: "#4e9cf4"
+    },
+    TR21: {
+      name: "Diyarbakir",
+      color: "#000080"
+    },
+    TR22: {
+      name: "Edirne",
+      color: "#b5bd0c"
+    },
+    TR23: {
+      name: "Elazig",
+      color: "#0a4f0a"
+    },
+    TR24: {
+      name: "Erzincan",
+      color: "#0a4f0a"
+    },
+    TR25: {
+      name: "Erzurum",
+      color: "#0a4f0a"
+    },
+    TR26: {
+      name: "Eskisehir",
+      color: "#DE3163"
+    },
+    TR27: {
+      name: "Gaziantep",
+      color: "#000080"
+    },
+    TR28: {
+      name: "Giresun",
+      color: "#008080"
+    },
+    TR29: {
+      name: "Gümüshane",
+      color: "#008080"
+    },
+    TR30: {
+      name: "Hakkari",
+      color: "#0a4f0a"
+    },
+    TR31: {
+      name: "Hatay",
+      color: "#5c0ade"
+    },
+    TR32: {
+      name: "Isparta",
+      color: "#5c0ade"
+    },
+    TR33: {
+      name: "Mersin",
+      color: "#5c0ade"
+    },
+    TR34: {
+      name: "Istanbul",
+      color: "#b5bd0c"
+    },
+    TR35: {
+      name: "Izmir",
+      color: "#4e9cf4"
+    },
+    TR36: {
+      name: "Kars",
+      color: "#0a4f0a"
+    },
+    TR37: {
+      name: "Kastamonu",
+      color: "#008080"
+    },
+    TR38: {
+      name: "Kayseri",
+      color: "#DE3163"
+    },
+    TR39: {
+      name: "Kirklareli",
+      color: "#b5bd0c"
+    },
+    TR40: {
+      name: "Kirsehir",
+      color: "#DE3163"
+    },
+    TR41: {
+      name: "Kocaeli",
+      color: "#b5bd0c"
+    },
+    TR42: {
+      name: "Konya",
+      color: "#DE3163"
+    },
+    TR43: {
+      name: "Kütahya",
+      color: "#4e9cf4"
+    },
+    TR44: {
+      name: "Malatya",
+      color: "#0a4f0a"
+    },
+    TR45: {
+      name: "Manisa",
+      color: "#4e9cf4"
+    },
+    TR46: {
+      name: "K. Maras",
+      color: "#5c0ade"
+    },
+    TR47: {
+      name: "Mardin",
+      color: "#000080"
+    },
+    TR48: {
+      name: "Mugla",
+      color: "#4e9cf4"
+    },
+    TR49: {
+      name: "Mus",
+      color: "#0a4f0a"
+    },
+    TR50: {
+      name: "Nevsehir",
+      color: "#DE3163"
+    },
+    TR51: {
+      name: "Nigde",
+      color: "#DE3163"
+    },
+    TR52: {
+      name: "Ordu",
+      color: "#008080"
+    },
+    TR53: {
+      name: "Rize",
+      color: "#008080"
+    },
+    TR54: {
+      name: "Sakarya",
+      color: "#b5bd0c"
+    },
+    TR55: {
+      name: "Samsun",
+      color: "#008080"
+    },
+    TR56: {
+      name: "Siirt",
+      color: "#000080"
+    },
+    TR57: {
+      name: "Sinop",
+      color: "#008080"
+    },
+    TR58: {
+      name: "Sivas",
+      color: "#DE3163"
+    },
+    TR59: {
+      name: "Tekirdag",
+      color: "#b5bd0c"
+    },
+    TR60: {
+      name: "Tokat",
+      color: "#008080"
+    },
+    TR61: {
+      name: "Trabzon",
+      color: "#008080"
+    },
+    TR62: {
+      name: "Tunceli",
+      color: "#0a4f0a"
+    },
+    TR63: {
+      name: "Sanliurfa",
+      color: "#000080"
+    },
+    TR64: {
+      name: "Usak",
+      color: "#4e9cf4"
+    },
+    TR65: {
+      name: "Van",
+      color: "#0a4f0a"
+    },
+    TR66: {
+      name: "Yozgat",
+      color: "#DE3163"
+    },
+    TR67: {
+      name: "Zinguldak",
+      color: "#008080"
+    },
+    TR68: {
+      name: "Aksaray",
+      color: "#DE3163"
+    },
+    TR69: {
+      name: "Bayburt",
+      color: "#008080"
+    },
+    TR70: {
+      name: "Karaman",
+      color: "#DE3163"
+    },
+    TR71: {
+      name: "Kinkkale",
+      color: "#DE3163"
+    },
+    TR72: {
+      name: "Batman",
+      color: "#000080"
+    },
+    TR73: {
+      name: "Sirnak",
+      color: "#000080"
+    },
+    TR74: {
+      name: "Bartın",
+      color: "#008080"
+    },
+    TR75: {
+      name: "Ardahan",
+      color: "#0a4f0a"
+    },
+    TR76: {
+      name: "Iğdir",
+      color: "#0a4f0a"
+    },
+    TR77: {
+      name: "Yalova",
+      color: "#b5bd0c"
+    },
+    TR78: {
+      name: "Karabük",
+      color: "#008080"
+    },
+    TR79: {
+      name: "Kilis",
+      color: "#000080"
+    },
+    TR80: {
+      name: "Osmaniye",
+      color: "#5c0ade"
+    },
+    TR81: {
+      name: "Düzce",
+      color: "#008080"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "39.94314007173174",
+      lng: "32.81411014968714",
+      color: "#ffffff",
+      size: "15",
+      opacity: "10",
+      description: "Ankara",
+      name: "Motacilla Alba",
+          type: "circle",
+          url: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/332193291/1200"
+    },
+    "1": {
+      name: "Chrysocyon brachyurus",
+      lat: "39.19919912047012",
+      lng: "39.40552444045883",
+      size: "15",
+      opacity: "10",
+      description: "Tunceli",
+      color: "#ffffff",
+        type: "circle",
+      url: "https://www.zoobrno.cz/wh/960-720/img/catalog/img/chrysocyon-brachyurus.jpg"
+    },
+    "2": {
+      name: "Corylus avellana",
+      size: "15",
+      opacity: "10",
+      color: "#ffffff",
+      lat: "40.958063626184305",
+      lng: "29.21939483484524",
+      type: "circle",
+        description: "İstanbul",
+        url: "https://www.treesandshrubsonline.org/site/assets/files/4997/corylus-avellana-11.jpg"
+    }
+  },
+  labels: {
+    TR01: {
+      name: "Adana",
+      parent_id: "TR01"
+    },
+    TR02: {
+      name: "Adiyaman",
+      parent_id: "TR02"
+    },
+    TR03: {
+      name: "Afyonkarahisar",
+      parent_id: "TR03"
+    },
+    TR04: {
+      name: "Agri",
+      parent_id: "TR04"
+    },
+    TR05: {
+      name: "Amasya",
+      parent_id: "TR05"
+    },
+    TR06: {
+      name: "Ankara",
+      parent_id: "TR06"
+    },
+    TR07: {
+      name: "Antalya",
+      parent_id: "TR07"
+    },
+    TR08: {
+      name: "Artvin",
+      parent_id: "TR08"
+    },
+    TR09: {
+      name: "Aydin",
+      parent_id: "TR09"
+    },
+    TR10: {
+      name: "Balikesir",
+      parent_id: "TR10"
+    },
+    TR11: {
+      name: "Bilecik",
+      parent_id: "TR11"
+    },
+    TR12: {
+      name: "Bingöl",
+      parent_id: "TR12"
+    },
+    TR13: {
+      name: "Bitlis",
+      parent_id: "TR13"
+    },
+    TR14: {
+      name: "Bolu",
+      parent_id: "TR14"
+    },
+    TR15: {
+      name: "Burdur",
+      parent_id: "TR15"
+    },
+    TR16: {
+      name: "Bursa",
+      parent_id: "TR16"
+    },
+    TR17: {
+      name: "Çanakkale",
+      parent_id: "TR17"
+    },
+    TR18: {
+      name: "Çankiri",
+      parent_id: "TR18"
+    },
+    TR19: {
+      name: "Çorum",
+      parent_id: "TR19"
+    },
+    TR20: {
+      name: "Denizli",
+      parent_id: "TR20"
+    },
+    TR21: {
+      name: "Diyarbakir",
+      parent_id: "TR21"
+    },
+    TR22: {
+      name: "Edirne",
+      parent_id: "TR22"
+    },
+    TR23: {
+      name: "Elazig",
+      parent_id: "TR23"
+    },
+    TR24: {
+      name: "Erzincan",
+      parent_id: "TR24"
+    },
+    TR25: {
+      name: "Erzurum",
+      parent_id: "TR25"
+    },
+    TR26: {
+      name: "Eskisehir",
+      parent_id: "TR26"
+    },
+    TR27: {
+      name: "Gaziantep",
+      parent_id: "TR27"
+    },
+    TR28: {
+      name: "Giresun",
+      parent_id: "TR28"
+    },
+    TR29: {
+      name: "Gümüshane",
+      parent_id: "TR29"
+    },
+    TR30: {
+      name: "Hakkari",
+      parent_id: "TR30"
+    },
+    TR31: {
+      name: "Hatay",
+      parent_id: "TR31"
+    },
+    TR32: {
+      name: "Isparta",
+      parent_id: "TR32"
+    },
+    TR33: {
+      name: "Mersin",
+      parent_id: "TR33"
+    },
+    TR34: {
+      name: "Istanbul",
+      parent_id: "TR34"
+    },
+    TR35: {
+      name: "Izmir",
+      parent_id: "TR35"
+    },
+    TR36: {
+      name: "Kars",
+      parent_id: "TR36"
+    },
+    TR37: {
+      name: "Kastamonu",
+      parent_id: "TR37"
+    },
+    TR38: {
+      name: "Kayseri",
+      parent_id: "TR38"
+    },
+    TR39: {
+      name: "Kirklareli",
+      parent_id: "TR39"
+    },
+    TR40: {
+      name: "Kirsehir",
+      parent_id: "TR40"
+    },
+    TR41: {
+      name: "Kocaeli",
+      parent_id: "TR41"
+    },
+    TR42: {
+      name: "Konya",
+      parent_id: "TR42"
+    },
+    TR43: {
+      name: "Kütahya",
+      parent_id: "TR43"
+    },
+    TR44: {
+      name: "Malatya",
+      parent_id: "TR44"
+    },
+    TR45: {
+      name: "Manisa",
+      parent_id: "TR45"
+    },
+    TR46: {
+      name: "K. Maras",
+      parent_id: "TR46"
+    },
+    TR47: {
+      name: "Mardin",
+      parent_id: "TR47"
+    },
+    TR48: {
+      name: "Mugla",
+      parent_id: "TR48"
+    },
+    TR49: {
+      name: "Mus",
+      parent_id: "TR49"
+    },
+    TR50: {
+      name: "Nevsehir",
+      parent_id: "TR50"
+    },
+    TR51: {
+      name: "Nigde",
+      parent_id: "TR51"
+    },
+    TR52: {
+      name: "Ordu",
+      parent_id: "TR52"
+    },
+    TR53: {
+      name: "Rize",
+      parent_id: "TR53"
+    },
+    TR54: {
+      name: "Sakarya",
+      parent_id: "TR54"
+    },
+    TR55: {
+      name: "Samsun",
+      parent_id: "TR55"
+    },
+    TR56: {
+      name: "Siirt",
+      parent_id: "TR56"
+    },
+    TR57: {
+      name: "Sinop",
+      parent_id: "TR57"
+    },
+    TR58: {
+      name: "Sivas",
+      parent_id: "TR58"
+    },
+    TR59: {
+      name: "Tekirdag",
+      parent_id: "TR59"
+    },
+    TR60: {
+      name: "Tokat",
+      parent_id: "TR60"
+    },
+    TR61: {
+      name: "Trabzon",
+      parent_id: "TR61"
+    },
+    TR62: {
+      name: "Tunceli",
+      parent_id: "TR62"
+    },
+    TR63: {
+      name: "Sanliurfa",
+      parent_id: "TR63"
+    },
+    TR64: {
+      name: "Usak",
+      parent_id: "TR64"
+    },
+    TR65: {
+      name: "Van",
+      parent_id: "TR65"
+    },
+    TR66: {
+      name: "Yozgat",
+      parent_id: "TR66"
+    },
+    TR67: {
+      name: "Zinguldak",
+      parent_id: "TR67"
+    },
+    TR68: {
+      name: "Aksaray",
+      parent_id: "TR68"
+    },
+    TR69: {
+      name: "Bayburt",
+      parent_id: "TR69"
+    },
+    TR70: {
+      name: "Karaman",
+      parent_id: "TR70"
+    },
+    TR71: {
+      name: "Kinkkale",
+      parent_id: "TR71"
+    },
+    TR72: {
+      name: "Batman",
+      parent_id: "TR72"
+    },
+    TR73: {
+      name: "Sirnak",
+      parent_id: "TR73"
+    },
+    TR74: {
+      name: "Bartın",
+      parent_id: "TR74"
+    },
+    TR75: {
+      name: "Ardahan",
+      parent_id: "TR75"
+    },
+    TR76: {
+      name: "Iğdir",
+      parent_id: "TR76"
+    },
+    TR77: {
+      name: "Yalova",
+      parent_id: "TR77"
+    },
+    TR78: {
+      name: "Karabük",
+      parent_id: "TR78"
+    },
+    TR79: {
+      name: "Kilis",
+      parent_id: "TR79"
+    },
+    TR80: {
+      name: "Osmaniye",
+      parent_id: "TR80"
+    },
+    TR81: {
+      name: "Düzce",
+      parent_id: "TR81"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
